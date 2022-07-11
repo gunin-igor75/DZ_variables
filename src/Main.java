71c594c5c1a53399f66a6370b4149ecbd1eb2725
Igor Gunin <gunin-igor75@yandex.ru>
2022-07-11 18:03:30 +0600

    UTF-8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Залача 1
+        // Р—Р°Р»Р°С‡Р° 1
         byte byteNumber = 10;
         short shortNumber = 100;
         long longNumber = 1000L;
@@ -10,38 +10,38 @@ public class Main {
         char charSymbol = 'A';
         boolean flag = true;
 
-        // Задача 2
+        // Р—Р°РґР°С‡Р° 2
         float weightBoxer1 = 78.2f;
         float weightBoxer2 = 82.7f;
         float totalWeight = weightBoxer1 + weightBoxer2;
-        System.out.println("Задача 2");
-        System.out.println("Общий вес двух бойцов " + totalWeight + " кг");
+        System.out.println("Р—Р°РґР°С‡Р° 2");
+        System.out.println("РћР±С‰РёР№ РІРµСЃ РґРІСѓС… Р±РѕР№С†РѕРІ " + totalWeight + " РєРі");
         float diffWeight = weightBoxer2 - weightBoxer1;
-        System.out.println("Разница между весами бойцов " + diffWeight + " кг");
+        System.out.println("Р Р°Р·РЅРёС†Р° РјРµР¶РґСѓ РІРµСЃР°РјРё Р±РѕР№С†РѕРІ " + diffWeight + " РєРі");
 
-        //Задача 3
+        //Р—Р°РґР°С‡Р° 3
         float weight_banana = 80f;
         float weight_milk = 105f;
         float weightIceCream = 100f;
         float weightEgg = 70f;
         float weightTotalGrams = weight_banana * 5 + weight_milk * 2 + weightIceCream * 2 + weightEgg * 4;
         float weightTotalKilogram = weightTotalGrams / 1000;
-        System.out.println("Задача 3");
-        System.out.println("Вес завтрака в килограммах " + weightTotalKilogram + " кг");
+        System.out.println("Р—Р°РґР°С‡Р° 3");
+        System.out.println("Р’РµСЃ Р·Р°РІС‚СЂР°РєР° РІ РєРёР»РѕРіСЂР°РјРјР°С… " + weightTotalKilogram + " РєРі");
 
-        //Задача 4
+        //Р—Р°РґР°С‡Р° 4
         int overWeight = 7000;
         int normWeight1 = 250;
         int normWeight2 = 500;
         int day1 = (overWeight + normWeight1 - 1) / normWeight1;
         int day2 = (overWeight + normWeight2 - 1) / normWeight2;
         int dayAvg = (day1 + day2 + 1) / 2;
-        System.out.println("Задача 4");
-        System.out.println("При уменьшении веса на 250 гр в  количество дней составит " + day1);
-        System.out.println("При уменьшении веса на 500 гр количество дней составит " + day2);
-        System.out.println("В среднем количество дней составит " + dayAvg);
+        System.out.println("Р—Р°РґР°С‡Р° 4");
+        System.out.println("РџСЂРё СѓРјРµРЅСЊС€РµРЅРёРё РІРµСЃР° РЅР° 250 РіСЂ РІ  РєРѕР»РёС‡РµСЃС‚РІРѕ РґРЅРµР№ СЃРѕСЃС‚Р°РІРёС‚ " + day1);
+        System.out.println("РџСЂРё СѓРјРµРЅСЊС€РµРЅРёРё РІРµСЃР° РЅР° 500 РіСЂ РєРѕР»РёС‡РµСЃС‚РІРѕ РґРЅРµР№ СЃРѕСЃС‚Р°РІРёС‚ " + day2);
+        System.out.println("Р’ СЃСЂРµРґРЅРµРј РєРѕР»РёС‡РµСЃС‚РІРѕ РґРЅРµР№ СЃРѕСЃС‚Р°РІРёС‚ " + dayAvg);
 
-        //Задача 5
+        //Р—Р°РґР°С‡Р° 5
         float salaryMasha = 67760f;
         float salaryDenis = 83690f;
         float salaryCristina = 76230f;
@@ -51,12 +51,12 @@ public class Main {
         float diffSalaryGodMasha = (newSalaryMasha - salaryMasha) * 12;
         float diffSalaryGodDinis = (newSalaryDenis - salaryDenis) * 12;
         float diffSalaryGodCristina = (newSalaryCristina - salaryCristina) * 12;
-        System.out.println("Задача 5");
-        System.out.println("Маша теперь получает " + newSalaryMasha + " рублей. " +
-                "Годовой доход вырос на " + diffSalaryGodMasha + " рублей");
-        System.out.println("Денис теперь получает " + newSalaryDenis + " рублей. " +
-                "Годовой доход вырос на " + diffSalaryGodDinis + " рублей");
-        System.out.println("Кристина теперь получает " + newSalaryCristina + " рублей. " +
-                "Годовой доход вырос на " + diffSalaryGodCristina + " рублей");
+        System.out.println("Р—Р°РґР°С‡Р° 5");
+        System.out.println("РњР°С€Р° С‚РµРїРµСЂСЊ РїРѕР»СѓС‡Р°РµС‚ " + newSalaryMasha + " СЂСѓР±Р»РµР№. " +
+                "Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РІС‹СЂРѕСЃ РЅР° " + diffSalaryGodMasha + " СЂСѓР±Р»РµР№");
+        System.out.println("Р”РµРЅРёСЃ С‚РµРїРµСЂСЊ РїРѕР»СѓС‡Р°РµС‚ " + newSalaryDenis + " СЂСѓР±Р»РµР№. " +
+                "Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РІС‹СЂРѕСЃ РЅР° " + diffSalaryGodDinis + " СЂСѓР±Р»РµР№");
+        System.out.println("РљСЂРёСЃС‚РёРЅР° С‚РµРїРµСЂСЊ РїРѕР»СѓС‡Р°РµС‚ " + newSalaryCristina + " СЂСѓР±Р»РµР№. " +
+                "Р“РѕРґРѕРІРѕР№ РґРѕС…РѕРґ РІС‹СЂРѕСЃ РЅР° " + diffSalaryGodCristina + " СЂСѓР±Р»РµР№");
     }
 }
